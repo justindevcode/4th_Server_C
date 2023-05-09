8d9c3fd5688cf7a7ad5b5a5445f74fe8d4aa7c26
minsub <chrkb1569@naver.com>
2023-05-09 22:25:06 +0900

    FEAT : Init Project

---
+++ 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/ApiServerApplication.java
@@ -0,0 +1,13 @@
+package umc.assac.API_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}

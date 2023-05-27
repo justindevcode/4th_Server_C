fbd7d3f0220d45c0cd16d367f6a48e2cdc4dfa50
minsub <chrkb1569@naver.com>
2023-05-27 19:38:53 +0900

    feat : Add findUserByUsername Method

--- 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/repository/user/UserRepository.java
+++ 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/repository/user/UserRepository.java
@@ -3,6 +3,9 @@ package umc.assac.API_Server.repository.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.assac.API_Server.domain.user.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
     boolean existsUserByUsernameOrEmail(String username, String email);
 }

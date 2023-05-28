bc32164a0d668128faf93f7213bc889b29a86126
minsub <chrkb1569@naver.com>
2023-05-28 13:31:33 +0900

    feat : Add OAuth Configuration

--- 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/config/SecurityConfig.java
+++ 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import umc.assac.API_Server.config.jwt.*;
+import umc.assac.API_Server.config.oauth.OAuthSuccessHandler;
+import umc.assac.API_Server.service.user.OAuthService;
 
 // Spring Security와 JWT를 활용한 보안 설정을 담당하는 파일
 @Configuration
@@ -19,6 +21,8 @@ public class SecurityConfig {
     private final JWTAccessDeniedHandler accessDeniedHandler;
     private final JWTAuthenticationEntryPointHandler authenticationEntryPointHandler;
     private final TokenProvider tokenProvider;
+    private final OAuthService oAuthService;
+    private final OAuthSuccessHandler oAuthSuccessHandler;
 
     private final String SWAGGER_PERMIT_ARRAY[] = {
             "/v2/api-docs",
@@ -54,11 +58,17 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/users/sign-up", "/api/users/sign-in", "/api/users/reIssue").permitAll()
+//                .antMatchers("/api/users/sign-up", "/api/users/sign-in", "/api/users/reIssue").permitAll()
+                .antMatchers("/**").permitAll()
                 .antMatchers(SWAGGER_PERMIT_ARRAY).permitAll()
                 .anyRequest().authenticated()
 
                 .and()
+                .oauth2Login()
+                .successHandler(oAuthSuccessHandler)
+                .userInfoEndpoint()
+                .userService(oAuthService)
+                .and().and()
                 .apply(new JwtSecurityConfig(tokenProvider))
                 .and().build();
     }

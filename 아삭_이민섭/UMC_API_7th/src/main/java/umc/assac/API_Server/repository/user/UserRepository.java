fc6436afd259dabe3b110de058b41d79f2c68ef8
minsub <chrkb1569@naver.com>
2023-05-28 13:30:10 +0900

    feat : Add existsUserByEmail Method

--- 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/repository/user/UserRepository.java
+++ 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/repository/user/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
     boolean existsUserByUsernameOrEmail(String username, String email);
+    boolean existsUserByEmail(String email);
 }

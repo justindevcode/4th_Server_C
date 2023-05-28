da1700179a5a1cb7f5060136964072e7fed84a63
minsub <chrkb1569@naver.com>
2023-05-28 13:27:53 +0900

    feat : Add provider field to User Domain

--- 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/domain/user/User.java
+++ 아삭_이민섭/UMC_API_7th/src/main/java/umc/assac/API_Server/domain/user/User.java
@@ -1,11 +1,17 @@
 package umc.assac.API_Server.domain.user;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 import umc.assac.API_Server.domain.base.BaseEntity;
+import umc.assac.API_Server.dto.oauth.UserProfile;
 import umc.assac.API_Server.dto.user.UserCreateDto;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
@@ -40,6 +46,9 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Authority authority; // 사용자의 권한
 
+    @Column(name = "provider")
+    private String provider; // 로그인을 수행하는 데에 사용한 서비스(ex) 구글, 카카오)
+
     public static User getUser(UserCreateDto createDto) {
         return User.builder()
                 .username(createDto.getUsername())
@@ -49,6 +58,19 @@ public class User extends BaseEntity {
                 .degree(36.5)
                 .isReport(false)
                 .authority(Authority.ROLE_USER)
+                .build();
+    }
+
+    public static User getUser(UserProfile userProfile, String username, String password) {
+        return User.builder()
+                .username(username)
+                .email(userProfile.getEmail())
+                .name(userProfile.getUsername())
+                .degree(36.5)
+                .isReport(false)
+                .password(password)
+                .authority(Authority.ROLE_USER)
+                .provider(userProfile.getProvider())
                 .build();
     }
 
